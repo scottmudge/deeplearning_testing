a52ac0b5da3aaf4b796af257a5cfe5327c2f1265
Scott Mudge <mail@scottmudge.com>
2018-11-08 00:21:48 -0500

    Prediction works

--- KerasTest1.py
+++ KerasTest1.py
@@ -6,12 +6,38 @@ from keras.datasets import mnist
 from keras import backend as K
 import cv2 as cv
 import tensorflow as tf
+import numpy as np
 from keras.backend.tensorflow_backend import set_session
 import utils.logger
 import utils.utility as utility
 from utils.logger import get_logger as logging
+from scipy import misc
+import glob
+import os
 
-import numpy as np
+
+img_rows = 28
+img_cols = 28
+
+
+def load_test_pngs(folder: str) -> list:
+    if not os.path.exists(folder):
+        raise FileNotFoundError("Supplied folder not found: {}".format(folder))
+
+    return_arr = np.ndarray((28, 28, 0))
+    x = 0
+    for image_path in glob.glob(folder + "/*.png"):
+        logging("Files").info("File {}: {}".format(x, image_path))
+        image = misc.imread(image_path, flatten=True)
+        return_arr = np.dstack((return_arr, image))
+        x+=1
+    return_arr = return_arr.swapaxes(0, 2)
+    return_arr = return_arr.reshape(return_arr.shape[0], 28, 28, 1)
+    logging("TEST").info("Image Shape: {}".format(return_arr.shape))
+    return_arr = return_arr.astype('float32')
+    return_arr /= 255.0
+    return return_arr
+
 
 if __name__ == '__main__':
 
@@ -37,9 +63,6 @@ if __name__ == '__main__':
     logging("Data").info("Initializing Data...")
     # Load pre-shuffled MNIST data
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    img_rows = 28
-    img_cols = 28
 
     # Should print 60000, 28, 28
     logging("Data").info("x_train shape: {}".format(x_train.shape))
@@ -80,10 +103,10 @@ if __name__ == '__main__':
     # 32 x 3 x 3 Conv layer with:
     #   > relu activation
     #   > input shape of 1 channel by 28 x 28 pixels
-    model.add(Conv2D(32, 3, 3, activation='relu', input_shape=input_shape))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 
     # Second Conv Layer
-    model.add(Conv2D(32, 3, 3, activation='relu'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
     # Max pooling layer
     model.add(MaxPooling2D(pool_size=(2, 2)))
     # Dropout to prevent over-fitting
@@ -103,21 +126,25 @@ if __name__ == '__main__':
     # Test if model weights already exists
     if utility.file_exists(model_fn):
         logging("Model").info("Loading model weights from disk:\n\t> Filename: {}".format(model_fn))
-        model = model.load_weights(model_fn)
+        model.load_weights(model_fn)
 
-    logging("Main").info("Done Loading!\n")
-    input("Press Enter to continue...")
+    logging("Model").info("Model Summary: \n")
+    model.summary()
 
-    logging("Model").info("Fitting model to data...")
-    model.fit(x_train, y_train, batch_size=32, nb_epoch=10, verbose=1)
+    logging("Main").info("Done Loading!")
+    inp = input("Press [1] For Training, or [2] For Testing")
 
-    score = model.evaluate(x_test, y_test, verbose=0)
-    logging("Score").info("Final score: \n\t> Loss: {}\n\t> Accuracy: {}".format(score[0], score[1]))
+    if inp == '1':
+        logging("Model").info("Fitting model to data...")
+        model.fit(x_train, y_train, batch_size=32, nb_epoch=10, verbose=1)
+        score = model.evaluate(x_test, y_test, verbose=0)
+        logging("Score").info("Final score: \n\t> Loss: {}\n\t> Accuracy: {}".format(score[0], score[1]))
 
-
-    logging("Model").info("Saving model...")
-    model.save(model_fn, True, True)
-
-
-
+        logging("Model").info("Saving model...")
+        model.save(model_fn, True, True)
+    elif inp == '2':
+        logging("Data").info("Loading test data...")
+        datas = load_test_pngs("{}/test_data/handwr_nums/".format(utility.get_root_directory()))
+        results = model.predict(datas)
+        logging("RESULTS").info("Results:\n\n{}".format(str(results)))
 
